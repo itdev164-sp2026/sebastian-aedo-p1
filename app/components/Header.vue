f50d54eba1adc8b632aba1d84eec1b5bdd56f2d2
Bysaedo <by.saedo@gmail.com>
2026-03-10 21:35:59 -0500

    Fixed header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,19 +1,21 @@
 <template>
-  <UHeader
+  <UHeader>
     <template #title>
       <div class="flex items-center gap-2 text-primary">
-      <UIcon name="streamline-plump:chef-toque-hat" class="size-10" />
-      <ULink to="/" class="flex items-center gap-2">
-      <div class="flex col">
-        <span class="text-2xl">AEDO'S KITCHEN</span>
-        <span class="text-xs text-muted">Sebastian Aedo Project 1</span>
-      </div>
-      </ULink>
+        <UIcon name="lucide:chef-hat" class="size-10" />
+        <ULink to="/" class="flex items-center gap-2">
+          <div class="flex flex-col">
+            <span class="text-2xl">AEDO'S KITCHEN</span>
+            <span class="text-xs text-muted">Sebastian Aedo Project 1</span>
+          </div>
+        </ULink>
       </div>
     </template>
     <template #right>
-      <ULink to="/recipes" icon="arcticons:reciper"></ULink>
-      <ULink to="/recipes/[slug]" icon="arcticons:recipe-keeper"></ULink>
+      <ULink to="/recipes" class="flex items-center gap-2">
+        <UIcon name="arcticons:reciper" class="size-10" />
+        <span>Recipes</span>
+      </ULink>
     </template>
   </UHeader>
 </template>
---
+++ app/pages/recipes.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <h1>All Recipes</h1>
+
+    <div v-for="recipe in recipes" :key="recipe.sys.id">
+      <ULink :to="`/recipes/${recipe.fields.slug}`">
+        {{ recipe.fields.title }}
+      </ULink>
+    </div>
+  </main>
+</template>
